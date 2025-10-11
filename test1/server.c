d16a77a6b2b86d09e25284c3bd5c6f9c3ae3fcf3
Clara Bordeau <cbordeau@f1r12s19.clusters.42paris.fr>
2025-10-11 14:45:27 +0200

    server

--- test1/server.c
+++ test1/server.c
@@ -6,7 +6,7 @@
 /*   By: cbordeau <cbordeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 11:48:19 by cbordeau          #+#    #+#             */
-/*   Updated: 2025/10/09 11:13:50 by aykrifa          ###   ########.fr       */
+/*   Updated: 2025/10/11 13:22:24 by cbordeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,27 @@ int main()
 	const size_t bufferSize = 1000;
 	const char *portNumber = "6969";
 	const int backlog = 1;
-	int serverSocket;
+	
 	struct addrinfo hints;
 	memset(&hints, 0, sizeof(struct addrinfo));
-	struct addrinfo *results;
-	struct addrinfo *record;
+
+
 	hints.ai_flags = AI_PASSIVE;
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
+
+	struct addrinfo *results;
 
 	if ((getaddrinfo(NULL, portNumber, &hints, &results)) != 0)
 	{
 		perror("Failed to translate client socket.");
 		exit(EXIT_FAILURE);
 	}
-	printf("Server socket translated.\n");
+	printf("Server socket translated into results.\n");
 
+	int serverSocket;
+	struct addrinfo *record;
 	for (record = results; record != NULL; record = record->ai_next)
 	{
 		serverSocket = socket(record->ai_family, record->ai_socktype, record->ai_protocol);
@@ -94,10 +98,14 @@ int main()
 			perror("Failed to receive message");
 			exit(EXIT_FAILURE);
 		}
-		printf("---------------RECEIVED MESSAGE------------------\n"
-				"%s\n"
-				"-------------END OF MESSAGE-----------\n", buffer);
-
+		printf("Message received\n");
+		char message[] = "Hi from server\n";
+		if (send(clientSocket, message, strlen(message), 0) == -1)
+		{
+			perror("Failed to perform cognitive recalibrication.");
+			exit(EXIT_FAILURE);
+		}
+		printf("Response sent\n");
 		close(clientSocket);
 		printf("Client socket closed.\n");
 	}
