9dd10950c8d824d916644bb0f12d34bab6f18bb9
Ayoub Krifa <krifa.ayoub@yahoo.com>
2025-10-21 15:50:50 +0200

    clean

--- classes/Locations/Locations.hpp
+++ classes/Locations/Locations.hpp
@@ -6,13 +6,14 @@
 /*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 12:06:50 by aykrifa           #+#    #+#             */
-/*   Updated: 2025/10/20 13:48:40 by aykrifa          ###   ########.fr       */
+/*   Updated: 2025/10/21 10:18:55 by aykrifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <vector>
+#include <map>
 #include <string>
 
 class	Location
@@ -26,10 +27,11 @@ class	Location
     	Location& operator=(const Location& src);
 	
 	private:
-		std::string		_root;
-		std::string		_key;
-		std::string		_index;
-		bool			_get;
-		bool			_post;
-		bool			_delete;
+		std::map<int, std::string>	_page;
+		std::string					_root;
+		std::string					_key;
+		std::string					_index;
+		bool						_get;
+		bool						_post;
+		bool						_delete;
 };
--- classes/Server/Server.hpp
+++ classes/Server/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 12:11:18 by aykrifa           #+#    #+#             */
-/*   Updated: 2025/10/20 13:48:15 by aykrifa          ###   ########.fr       */
+/*   Updated: 2025/10/21 10:07:26 by aykrifa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,9 @@ class	Server
 
 		Server(void);
 		Server(const Server& src);
-
 		~Server();
 
 		Server& operator=(const Server& server);
-
-		void print();
 
 	private:
 		std::string				_name;
