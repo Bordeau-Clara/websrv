c15aa84264f43fd015d437df370c92bbe2e6b95b
Aykrifa <aykrifa@student.42.fr>
2025-11-30 14:49:13 +0100

    clean

--- code/classes/Server/Server.hpp
+++ code/classes/Server/Server.hpp
@@ -23,25 +23,20 @@ class	Server
 		~Server(void);
 		Server	operator=(const Server&);
 
-	public:
+		static const int				adress_family = AF_INET;
+
 		const std::map<std::string, Location>	&getLocations(void) const;
 		void									setLocationsMap(const std::map<std::string, Location>&);
 		void									addLocations(std::string&, Location&);
-	private:
-		std::map<std::string, Location>	_locations;
 
-	public:
 		unsigned short int						getPort(void) const;
 		void									setPort(unsigned short int);
-	private:
-		unsigned short int				_port;
 
-	public:
 		unsigned int							getInterface(void) const;
 		void									setInterface(unsigned int);
-	private:
-		unsigned int					_interface;
 	
-	public:
-		static const int				adress_family = AF_INET;
+	private:
+		std::map<std::string, Location>	_locations;
+		unsigned short int				_port;
+		unsigned int					_interface;
 };
