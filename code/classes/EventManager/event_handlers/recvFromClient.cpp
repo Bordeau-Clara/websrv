043d11ca286f8c33ffa7c5ccee19afb168ed8e8f
Aykrifa <aykrifa@student.42.fr>
2025-12-21 17:21:52 +0100

    refactor if statements

--- code/classes/EventManager/event_handlers/recvFromClient.cpp
+++ code/classes/EventManager/event_handlers/recvFromClient.cpp
@@ -39,36 +39,34 @@ void	EventManager::recvFromClient(void)
 	// si reception
 	// si aucun element est recu
 	// CECI n'est pas tres propre
-	if (!recvBuffer(client)) // si rien recu
+	if (!recvBuffer(client)) // if nothing has been received
 		return ;
 	client.parseBuffer();
-	// si parsing est fini
+	if (!client.isState(EXEC))// if parsing is not finished
+		return ;
 	// (should be only send because error is send)
-	if (client.isState(EXEC))
+	// streams.print(LOG_EVENT) << "[CLIENT switching sending mode]" << std::endl
+
+	// checke si la reponse attendue est celle dune cgi
+	if (client.isState(CGI))// && !client.isState(ERROR)
 	{
-		// streams.print(LOG_EVENT) << "[CLIENT switching sending mode]" << std::endl
+		//mettre l'event en dormant (EPOLLONESHOT) faut il d'abord le mettre en EPOLLOUT?
+		//-> .events = 0 est plus propre car EPOLLONESHOT est fais pour bloquer apres la reception d'un event
+		//comme client va etre mis dans l'event de la cgi(ou inversement) on aura le fd pour le reactiver
+		//lancer cgi et add l'event en EPOLLIN
+		//quand cgi finit on le met dans la rep du client et on le reactive et on le passe en EPOLLOUT
+		//
+		//client.startCgi();
+		//create pipe fork and send optional body through new pipe and fork here??
 
-		// checke si la reponse attendue est celle dune cgi
-		if (client.isState(CGI))// && !client.isState(ERROR)
-		{
-			//mettre l'event en dormant (EPOLLONESHOT) faut il d'abord le mettre en EPOLLOUT?
-			//-> .events = 0 est plus propre car EPOLLONESHOT est fais pour bloquer apres la reception d'un event
-			//comme client va etre mis dans l'event de la cgi(ou inversement) on aura le fd pour le reactiver
-			//lancer cgi et add l'event en EPOLLIN
-			//quand cgi finit on le met dans la rep du client et on le reactive et on le passe en EPOLLOUT
-			//
-			//client.startCgi();
-			//create pipe fork and send optional body through new pipe and fork here??
-
-			// ecoute le pipe cgi
-			EventAdd(client.getCgi()._responsePipe[1], EPOLLIN, &client.getCgi());
-			// mute les envois clients
-			EventModify(client.fd, 0, &client);
-		}
-		else // passe en emission
-		{
-			client.generateResponse();
-			EventModify(client.fd, EPOLLOUT, &client);
-		}
+		// ecoute le pipe cgi
+		EventAdd(client.getCgi()._responsePipe[1], EPOLLIN, &client.getCgi());
+		// mute les envois clients
+		EventModify(client.fd, 0, &client);
+	}
+	else // passe en emission
+	{
+		client.generateResponse();
+		EventModify(client.fd, EPOLLOUT, &client);
 	}
 }
