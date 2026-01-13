706e02ab9fa7923be4e0cac2086db86b89caf0b0
Ayoub Krifa <krifa.ayoub@yahoo.com>
2026-01-13 14:43:45 +0100

    new handler for stdin

---
+++ code/classes/EventManager/event_handlers/handleStdin.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   serverAcceptClient.cpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aykrifa <aykrifa@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/14 14:24:34 by aykrifa           #+#    #+#             */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "EventManager.hpp"
+
+#include <fcntl.h>
+#include <unistd.h>
+
+void	EventManager::handleStdin(void)
+{
+	static char	buffer[BUFFER_SIZE + 1];
+
+	ssize_t	rbytes = read(0, buffer, BUFFER_SIZE);
+	if (!rbytes)
+		_alive = false;
+}
