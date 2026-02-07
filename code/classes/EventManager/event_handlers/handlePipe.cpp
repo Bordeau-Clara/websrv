f148eabfc4e062b2f187f5921afbc482a230704c
Aykrifa <aykrifa@student.42.fr>
2026-02-07 14:43:10 +0100

    monitor handle pipe clean

--- code/classes/EventManager/event_handlers/handlePipe.cpp
+++ code/classes/EventManager/event_handlers/handlePipe.cpp
@@ -12,6 +12,7 @@
 #include "EventManager.hpp"
 
 #include <cerrno>
+#include <cstring>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -21,35 +22,41 @@
 #include "Request.hpp"
 #include "Cgi.hpp"
 #include "colors.hpp"
+#include "statusCodes.hpp"
 #include "string.hpp"
 #include "helpers.hpp"
 
 void	EventManager::handlePipe()
 {
-	Monitor.printNewLine(VIVID_YELLOW + "Handling A PIPE ..."  + RESET);
 	Cgi &cgi = *(Cgi *)getPtr();
 	static char buffer[BUFFER_SIZE] = {0};
 	ssize_t count = read(cgi._responsePipe[0], buffer, sizeof(buffer));
-	if (count == -1)
-		throw (std::runtime_error("RECV KO"));
-	if (count)
+	if (count > 0)
 	{
 		cgi._buffer.append(buffer, count);
-		Monitor.printNewLine(BRIGHT_YELLOW + "RECV FROM PIPE " + nbrToString(count) + " BYTES !" + RESET);
+		Monitor.printNewLine(BRIGHT_YELLOW + "READ from cgi " + nbrToString(count) + " BYTES !" + RESET);
 		/**/streams.get(LOG_EVENT) << "RECEIVED:" +cgi._buffer << std::endl
 			/**/<< std::endl;
 		return ;
 	}
-	// else -> eof
-	Monitor.printNewLine(ORANGE + "ENDOF PIPE"  + RESET);
-	//treat info and put into cgi.request.response
-	cgi.parseBuffer();
+	if (count == -1) // read error : cgi broken ?
+	{
+		Monitor.printNewLine(VIVID_RED + "Unexpected broken CGI pipe: "+ strerror(errno) + RESET);
+		cgi._client->setError(Status(GATEWAY_TIMEOUT, 504));
+		cgi._client->buildErrorResponse();
+	}
+	else // else -> eof
+	{
+		Monitor.printNewLine(ORANGE + "CGI successfully ended"  + RESET);
+		//treat info and put into cgi.request.response
+		cgi.parseBuffer();
+		/**/streams.get(LOG_EVENT) << "{FD}" << cgi._client->fd << std::endl
+			/**/<< std::endl;
+		cgi._client->setState(EXEC);
+	}
 	//DEL event cgi
 	EventDelete(cgi._responsePipe[0]);
 	close(cgi._responsePipe[0]);
-	/**/streams.get(LOG_EVENT) << "{FD}" << cgi._client->fd << std::endl
-		/**/<< std::endl;
-	cgi._client->setState(EXEC);
 	//MOD request back to EPOLLOUT
 	EventModify(cgi._client->fd, EPOLLOUT, cgi._client);
 }
