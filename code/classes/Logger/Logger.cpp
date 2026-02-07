164ae21b682f8733df9e0ddffb1ecd2465916340
Aykrifa <aykrifa@student.42.fr>
2026-02-07 15:37:28 +0100

    Reset color in each line of logger

--- code/classes/Logger/Logger.cpp
+++ code/classes/Logger/Logger.cpp
@@ -10,6 +10,7 @@
 /* ************************************************************************** */
 
 #include "Logger.hpp"
+#include "colors.hpp"
 std::string strTimestamp(void);
 
 // Constructeur par défaut : statut vide
@@ -37,7 +38,7 @@ Logger::Logger(const String& defaultStatus, std::ostream& output) : _status_line
 Logger::~Logger()
 {
     // Optionnel : Nettoyer la ligne de statut à la fermeture
-    this->_stream << ANSI_CLEAR_LINE << ANSI_MOVE_START << std::endl;
+    this->_stream << ANSI_CLEAR_LINE << ANSI_MOVE_START  << RESET << std::endl;
 }
 
 // Helper privé : Efface la ligne courante et réécrit le status
@@ -47,7 +48,7 @@ void Logger::_refreshStatusLine(void) const
     // 2. \r      : Remet le curseur au début (au cas où)
     // 3. affiche le status
     // 4. flush   : Force l'affichage sans attendre un \n
-    this->_stream << ANSI_CLEAR_LINE << ANSI_MOVE_START << strTimestamp() + _status_line << std::flush;
+    this->_stream << ANSI_CLEAR_LINE << ANSI_MOVE_START << strTimestamp() + _status_line << RESET << std::flush;
 }
 
 void Logger::editStatusLine(const String& newStatus)
@@ -62,10 +63,10 @@ void Logger::printNewLine(const String& msg) const
     this->_stream << ANSI_CLEAR_LINE << ANSI_MOVE_START;
     
     // 2. On imprime le nouveau log (qui va faire descendre l'écran d'une ligne)
-    this->_stream << strTimestamp() + msg << std::endl;
+    this->_stream << strTimestamp() + msg << RESET << std::endl;
     
     // 3. On réimprime la ligne de statut en bas
-    this->_stream << strTimestamp() + _status_line << std::flush;
+    this->_stream << strTimestamp() + _status_line  << RESET << std::flush;
 }
 
 void Logger::popStatus(const String& nextStatus)
