90b429d07c7d32a12c284d65c7f5185edcc6d0ee
Aykrifa <aykrifa@student.42.fr>
2026-02-09 20:37:59 +0100

    tester $$ clean

--- code/gemini_errorCodeCreator.sh
+++
@@ -1,4 +0,0 @@
-grep -E '"[45][0-9]{2}' includes/statusCodes.hpp  | sed -E 's/.*"(([0-9]{3}) [^"]*)".*/\2|\1/' | while IFS="|" read -r code msg; do
-    echo "<html><body style='font-family:sans-serif; text-align:center; padding-top:20%'><h1>$msg</h1></body></html>" > classes/Location/default_error_pages/"${code}.html" 
-    echo "Fichier ${code}.html créé."
-done
