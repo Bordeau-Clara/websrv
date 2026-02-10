035e1358620df650bf9de99740d7e6f6e0e0e069
Clara <clara.bordeau@gmail.com>
2026-02-10 12:26:12 +0100

    test syntax

--- bad_request_tester/notImplemeted.sh
+++ bad_request_tester/notImplemeted.sh
@@ -1,1 +1,4 @@
-printf "GET / HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding:gzip\r\n\r\n" | nc localhost 8080
+(printf "GET / HTTP/1.1\r\n";
+printf "Host: localhost\r\n";
+printf "Transfer-encoding:gzip\r\n";
+printf "\r\n") | nc localhost 8080
